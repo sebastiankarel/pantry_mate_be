e505df770dd26e03fbbab5420704405b515b44ad
Sebastian Lembcke <sebastiankarel.lembcke@googlemail.com>
2025-06-28 17:55:42 +0200

    Initial commit

---
+++ src/test/kotlin/de/slapps/pantry_mate/PantryMateApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.slapps.pantry_mate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PantryMateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
